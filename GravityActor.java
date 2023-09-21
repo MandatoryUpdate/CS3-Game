8c5b5adb083d1492bf660059eb37054f586edce7
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2023-09-21 14:25:45 -0500

    Update GravityActor.java

--- GravityActor.java
+++ GravityActor.java
@@ -2,29 +2,30 @@
 import mayflower.*;
 public class GravityActor extends Actor
 {
-    private int gravSpeed = 0;
+    private double gravSpeed = 0;
     
     public GravityActor()
     {
         
     }
     public void act(){        
-        gravSpeed+=0.5;
+        gravSpeed+=0.1;
         setLocation(getX(),getY()+gravSpeed);
         if(isBlocked())
+            gravSpeed = 0;
+            setLocation(getX(),getY()-1);
             setLocation(getX(),getY()-1);
     }
     
     public boolean isBlocked(){
         if(this.isTouching(Block.class)){
-            gravSpeed = 0;
             return true;
         }
         return false;
     }
     public boolean isFalling(){
         boolean ret;
-        setLocation(getX(), getY() + gravSpeed);
+        setLocation(getX(), getY() + 1);
         ret = isTouching(Block.class);
         setLocation(getX(), getY() - 1);
         return !ret;
