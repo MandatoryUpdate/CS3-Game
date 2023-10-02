359c70a22c56f3efac6b6f7c5bbb8b205a050aee
armaankhatwani <143357365+armaankhatwani@users.noreply.github.com>
2023-10-02 15:35:09 -0500

    Update GameEndLose.java

--- GameEndLose.java
+++ GameEndLose.java
@@ -1,18 +1,24 @@
 import mayflower.*;
 public class GameEndLose extends World 
 {
-    private StartButton restart = new StartButton("img/dog/Fall (2).png","img/dog/Dead (4).png");
+    private StartButton restart = new StartButton("img/Button/restartButton.png","img/Button/restartButton.png");
+    private EndButton exit = new EndButton("img/Button/exitButton.png", "img/Button/exitButton.png");
     
     public GameEndLose() 
     {
-    	setBackground("img/BG/lose.png");
-    	
+        setBackground("lose.png");
+        
     }
     
     public void act()
     {
-        showText("You Lost!",335, 200, Color.BLACK);
-        addObject(restart, 300, 200);
+        showText("GAME OVER You Lost!",200, 200, Color.BLACK);
+        
+        addObject(restart, 25, 300);
+        restart.scale(200,100);
+        
+        addObject(exit, 300, 300);
+        exit.scale(200,100);
     }
     
 }
