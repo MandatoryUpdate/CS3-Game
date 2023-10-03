e553a81dfeae1b9185a86be3994c80250509d064
JacobWeikum <145498545+JacobWeikum@users.noreply.github.com>
2023-10-03 12:33:44 -0500

    Update Level.java

--- Level.java
+++ Level.java
@@ -15,11 +15,10 @@ public class Level extends World
     public void buildScreen(int part){
         for(int r=0;r<tiles[part].length;r++){
             for(int c=0;c<tiles[part][0].length;c++){
-                System.out.println(tiles[part][r][c]);
-                if(tiles[part][r][c].equals("ground")){
-                    System.out.println("read");
+                if(tiles[part][r][c].equals("ground"))
                     addObject(new Block(), c*61, r*32);
-                }
+                if(tiles[part][r][c].length()>0 && tiles[part][r][c].substring(0,1).equals("p"))
+                    addObject(new Punk(Integer.parseInt(tiles[part][r][c].substring(5,8)),Integer.parseInt(tiles[part][r][c].substring(9,12)),Integer.parseInt(tiles[part][r][c].substring(13,15))),c*100, r*100);
             }
         }
     }
