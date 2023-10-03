00f87a816cba7a437d1ec8a9ed1f7638fcdbae18
JacobWeikum <145498545+JacobWeikum@users.noreply.github.com>
2023-10-03 12:54:02 -0500

    12X16 and block update

--- Block.java
+++ Block.java
@@ -3,7 +3,10 @@ import mayflower.*;
 public class Block extends Actor
 {
     public Block(){
-        setImage("img/Tiles/2.png");
+        MayflowerImage block = new MayflowerImage("img/1 Tiles/Tile_02.png");
+        block.scale(50,50);
+        setImage(block);
+        
     }
     
     public void act(){
--- Level.java
+++ Level.java
@@ -1,5 +1,6 @@
 
 import mayflower.*;
+import java.util.*;
 public class Level extends World
 {
     private String[][][] tiles;
@@ -15,8 +16,9 @@ public class Level extends World
     public void buildScreen(int part){
         for(int r=0;r<tiles[part].length;r++){
             for(int c=0;c<tiles[part][0].length;c++){
+                System.out.println(tiles[part][r][c]);
                 if(tiles[part][r][c].equals("ground"))
-                    addObject(new Block(), c*61, r*32);
+                    addObject(new Block(), c*50, r*50);
                 if(tiles[part][r][c].length()>0 && tiles[part][r][c].substring(0,1).equals("p"))
                     addObject(new Punk(Integer.parseInt(tiles[part][r][c].substring(5,8)),Integer.parseInt(tiles[part][r][c].substring(9,12)),Integer.parseInt(tiles[part][r][c].substring(13,15))),c*100, r*100);
             }
@@ -25,11 +27,13 @@ public class Level extends World
     public void increment(){
         part++;
         removeObjects(getObjects(Block.class));
+        removeObjects(getObjects(Enemy.class));
         buildScreen(part);
     }
     public void decrease(){
         part--;
         removeObjects(getObjects(Block.class));
+        removeObjects(getObjects(Enemy.class));
         buildScreen(part);
     }
     public int getPart(){
