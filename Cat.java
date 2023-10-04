4821f0fbe2f686fd5ec855ad8a3176dc55286c97
JacobWeikum <145498545+JacobWeikum@users.noreply.github.com>
2023-10-04 14:44:37 -0500

    Add files via upload

--- Cat.java
+++ Cat.java
@@ -5,55 +5,57 @@ public class Cat extends MovableAnimatedActor
     private static int score = 0;
     private static int lives = 3;
     public Cat(){
-        String[] frames = new String[10];
-        for(int i=0;i<10;i++){
-            frames[i] = "img/cat/Walk ("+(i+1)+").png";
+        String[] frames = new String[6];
+        for(int i=0;i<6;i++){
+            frames[i] = "img/3 Cyborg/Run/run"+i+".png";
         }
-        Animation walkR = new Animation(50,frames);
-        walkR.resize(100,87);
-        walkR.setBounds(18,4,54,80);
+        Animation walkR = new Animation(100000000,frames);
+        walkR.resize(50,50);
+        // walkR.setBounds(18,4,54,80);
         
-        Animation walkL = new Animation(50,frames);
-        walkL.resize(100,87);
+        Animation walkL = new Animation(100000000,frames);
+        walkL.resize(50,50);
         walkL.flipHorizontally();
-        walkL.setBounds(28,4,54,80);
-        for(int i=0;i<10;i++){
-            frames[i] = "img/cat/Idle ("+(i+1)+").png";
-        }
-        Animation idleR = new Animation(50,frames);
-        idleR.resize(100,87);
-        idleR.setBounds(18,4,54,80);
+        // walkL.setBounds(28,4,54,80);
         
-        Animation idleL = new Animation(50,frames);
-        idleL.resize(100,87);
+        frames = new String[4];
+        for(int i=0;i<4;i++){
+            frames[i] = "img/3 Cyborg/Idle/idle"+i+".png";
+        }
+        Animation idleR = new Animation(100000000,frames);
+        idleR.resize(50,50);
+        // idleR.setBounds(18,4,54,80);
+        
+        Animation idleL = new Animation(100000000,frames);
+        idleL.resize(50,50);
         idleL.flipHorizontally();
-        idleL.setBounds(28,4,54,80);
+        // idleL.setBounds(28,4,54,80);
         
-        frames = new String[8];
-        for(int i=0;i<8;i++){
-            frames[i] = "img/cat/Fall ("+(i+1)+").png";
+        frames = new String[4];
+        for(int i=0;i<4;i++){
+            frames[i] = "img/3 Cyborg/Jump/jump"+i+".png";
         }
-        Animation fallR = new Animation(50,frames);
-        fallR.resize(100,87);
-        fallR.setBounds(18,4,54,80);
+        Animation fallR = new Animation(100000000,frames);
+        fallR.resize(50,50);
+        // fallR.setBounds(18,4,54,80);
 
-        Animation fallL = new Animation(50,frames);
-        fallL.resize(100,87);
+        Animation fallL = new Animation(100000000,frames);
+        fallL.resize(50,50);
         fallL.flipHorizontally();
-        fallL.setBounds(28,4,54,80);
+        // fallL.setBounds(28,4,54,80);
         
-        frames = new String[8];
-        for(int i=0;i<8;i++){
-            frames[i] = "img/cat/Jump ("+(i+1)+").png";
+        frames = new String[4];
+        for(int i=0;i<4;i++){
+            frames[i] = "img/3 Cyborg/Jump/jump"+i+".png";
         }
-        Animation jumpR = new Animation(50,frames);
-        jumpR.resize(100,87);
-        jumpR.setBounds(18,4,54,80);
+        Animation jumpR = new Animation(100000000,frames);
+        jumpR.resize(50,50);
+        // jumpR.setBounds(18,4,54,80);
 
-        Animation jumpL = new Animation(50,frames);
-        jumpL.resize(100,87);
+        Animation jumpL = new Animation(100000000,frames);
+        jumpL.resize(50,50);
         jumpL.flipHorizontally();
-        jumpL.setBounds(28,4,54,80);
+        // jumpL.setBounds(28,4,54,80);
         
         setWalkRightAnimation(walkR);
         setWalkLeftAnimation(walkL);
