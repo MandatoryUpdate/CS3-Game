f40d6b8d4d2b3cef198194a395beead98c72610d
armaankhatwani <143357365+armaankhatwani@users.noreply.github.com>
2023-10-04 14:51:41 -0500

    Update TitleScreen.java

--- TitleScreen.java
+++ TitleScreen.java
@@ -2,8 +2,8 @@
 import mayflower.*;
 public class TitleScreen extends World
 {
-    private StartButton start = new StartButton("img/Button/playButton.png","img/Button/playButton.png");
-    private EndButton end = new EndButton("img/Button/exitButton.png","img/Button/exitButton.png");
+    private StartButton start = new StartButton("img/Button/playButtonHighlight.png","img/Button/playButton.png");
+    private EndButton end = new EndButton("img/Button/exitButtonHighlight.png","img/Button/exitButton.png");
     public TitleScreen(){
         setBackground("img/BG/BG.png");
         showText("CS3 Game: ", 100, 300, Color.BLACK);
