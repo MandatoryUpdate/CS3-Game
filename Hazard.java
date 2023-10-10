d8cb510d8f2688251f636ba7983f23ebeea8cdd1
JacobWeikum <145498545+JacobWeikum@users.noreply.github.com>
2023-10-09 20:57:19 -0500

    Hazard Update

--- Hazard.java
+++ Hazard.java
@@ -1,27 +1,16 @@
 import mayflower.*;
 public class Hazard extends Actor
 {
-    private int x;
-    private int y;
-    public Hazard(int startx, int starty)
-    {
-       x = startx;
-       y = starty;
+    public Hazard(){
+        
     }
 
-    
     public void act()
     {
         if(isTouching(Cat.class)){
           Cat a = getOneIntersectingObject(Cat.class);
-          World w = getWorld();
           a.decreaseLives();
-          a.setLocation(x,y);
-          if(a.getLives()==0){
-              Mayflower.setWorld(new GameEndLose());
-            }
         }  
-        
     }
 }
 
