4ca0781d3976999a1b07f6bbda3d0ebd23424a2d
JacobWeikum <145498545+JacobWeikum@users.noreply.github.com>
2023-10-09 21:18:27 -0500

    Update GameEndWin.java

--- GameEndWin.java
+++ GameEndWin.java
@@ -2,16 +2,19 @@ import mayflower.*;
 
 public class GameEndWin extends World 
 {
-    
+    private RestartButton restart = new RestartButton("img/Button/restartButton.png","img/Button/restartButton.png");
+    private EndButton exit = new EndButton("img/Button/exitButton.png", "img/Button/exitButton.png");
     public GameEndWin() 
     {
-    	setBackground("img/bg/lose.png");
-    	
+        setBackground("img/bg/lose.png");
+        showText("You Win!",335, 200, Color.BLACK);
+        addObject(restart, 25, 300);
+        restart.scale(200,100);
+        addObject(exit, 300, 300);
+        exit.scale(200,100);
     }
     
-    public void act()
-    {
-        showText("You Win!",335, 200, Color.BLACK);
+    public void act(){
     }
     
 }
